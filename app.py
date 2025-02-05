459ef1d1b7dbf7b49641bcd230220d5cc3aff44c
Abenezer Boka <33054939+AB-Solution@users.noreply.github.com>
2025-02-06 01:50:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def submit():
     return jsonify({'status': 'success', 'message': 'Data sent to Telegram'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
